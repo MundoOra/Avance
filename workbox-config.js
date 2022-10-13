1ba3bf354044b05a45ee9dea33fe1319365f389a
MundoOra <mundoorar@gmail.com>
2022-10-13 10:16:21 -0500

    Initial commit

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: 'C:/Users/mundo/Downloads/avance',
+	globPatterns: [
+		'**/*.{mp3,html,jpg,css,json}'
+	],
+	swDest: 'C:/Users/mundo/Downloads/avance/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
